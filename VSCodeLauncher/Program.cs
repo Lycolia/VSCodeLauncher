40942b90ad568a3a33527b641c19ee68a9c58bcb
Lycolia Rizzim <lycolia@lycolia.info>
2023-03-23 00:50:00 +0900

    fix comments

--- VSCodeLauncher/Program.cs
+++ VSCodeLauncher/Program.cs
@@ -3,26 +3,13 @@ using VSCodeLauncher.Lib.CommandGenerator;
 using VSCodeLauncher.Lib.CommandLineParser;
 using VSCodeLauncher.Lib.Config;
 /**
-* 処理
-* 
-* config.jsonをロード
-*  ファイルがなければエラー
-*  フォーマット不正もエラー
-* フルパスがUNCパスでなければローカル
-*  codeを普通に蹴る
-* UNCであればリモート
-*  <config.type>を見る
-*      SSHならssh-remote+<config.hostName>
-*      WSLならwsl+<config.hostName>
-* フルパス先頭から<config.explorerPrefix>を削る
-* フルパス先頭に<appendPrefix>を足す
-* 以下の方式でcodeを蹴る
-* <config.CodePath> <MODE+HOST_NAME> <FULL_PATH>
-* 
-* このアプリ自体は以下の形式で蹴る
-*  VSCodeLauncher.exe path\to\config.json <FULL_PATH>
+* usage
+*   VSCodeLauncher.exe path\to\config.json <FULL_PATH>
 *  
-* 絶対パス以外は許容しない
+* TODO
+*   configのパス指定
+*   自動テスト
+*   Windows, WSL, SSHの動作確認
 * 
 */
 
@@ -52,64 +39,4 @@ foreach (var argv in cg.GenerateCommand()) {
 }
 
 Process.Start(psi);
-
-
-//try {
-//    var configFile = ConfigFile.LoadConfig("./config.json");
-//    if (configFile == null) {
-//        return;
-//    }
-//    var cg = new CommandGenerator(fullPath, configFile);
-//    var cmd = cg.GenerateCommand();
-//    //var t = configFile.;
-//} catch (Exception ex) {
-//    Console.WriteLine(ex);
-//}
-
-// UNCパス判定
-// 起動コマンド生成
-// 起動
-
-Console.WriteLine("");
-
-//var cmdArgs = Environment.GetCommandLineArgs();
-
-//var CodeBinPath = cmdArgs[1];
-///**
-// * ssh-remote+<remoteHost>
-// * wsl+<distro name>
-// */
-//var OpenMode = cmdArgs[2];
-//var RemotePathPrefix = cmdArgs[3];
-//var FullPath = cmdArgs[4];
-
-//var RegEscapedRemotePathPrefix = Regex.Escape(RemotePathPrefix);
-//var IsRemotePath = Regex.IsMatch(FullPath, $"^{RegEscapedRemotePathPrefix}");
-
-
-//var psi = new ProcessStartInfo();
-//psi.FileName = CodeBinPath;
-//psi.WindowStyle = ProcessWindowStyle.Hidden;
-
-//Console.WriteLine(CodeBinPath);
-//Console.WriteLine(OpenMode);
-//Console.WriteLine(RemotePathPrefix);
-//Console.WriteLine(FullPath);
-
-//if (IsRemotePath) {
-
-//    // open with WSL
-//    var wslPath = Regex.Replace(FullPath, $"^{RegEscapedRemotePathPrefix}", "");
-//    Console.WriteLine("wslPath" + wslPath);
-//    Console.ReadLine();
-
-//    psi.ArgumentList.Add("--remote");
-//    psi.ArgumentList.Add(OpenMode);
-//    psi.ArgumentList.Add(wslPath.Replace(@"\", "/"));
-//    Process.Start(psi);
-//} else {
-//    // open with Windows
-//    psi.ArgumentList.Add(FullPath);
-//    Process.Start(psi);
-//}
 
