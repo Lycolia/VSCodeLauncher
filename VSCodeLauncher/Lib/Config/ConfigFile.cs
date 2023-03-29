ed2ead069607a124b1696d48d5552031b142cfc7
Lycolia Rizzim <lycolia@lycolia.info>
2023-03-29 22:39:01 +0900

    fix find config path

--- VSCodeLauncher/Lib/Config/ConfigFile.cs
+++ VSCodeLauncher/Lib/Config/ConfigFile.cs
@@ -1,9 +1,19 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 
 namespace VSCodeLauncher.Lib.Config {
     public class ConfigFile {
         public static ConfigBase LoadConfig(string configPath) {
-            var configFullPath = Path.Combine(Directory.GetCurrentDirectory(), configPath);
+            var assembly = Assembly.GetEntryAssembly();
+            if (assembly == null) {
+                throw new Exception("An unexpected exception has occurred");
+            }
+            var executePath = Path.GetDirectoryName(assembly.Location);
+            if (executePath == null) {
+                throw new Exception("An unexpected exception has occurred");
+            }
+
+            var configFullPath = Path.Combine(executePath, configPath);
 
             if (File.Exists(configFullPath)) {
                 var jsonString = File.ReadAllText(configFullPath);
