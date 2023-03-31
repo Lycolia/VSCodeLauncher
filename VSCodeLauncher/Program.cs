f6d7277b522bc023443289df609a632d0134ab5e
Lycolia Rizzim <lycolia@lycolia.info>
2023-03-31 21:10:15 +0900

    add useful error handle

--- VSCodeLauncher/Program.cs
+++ VSCodeLauncher/Program.cs
@@ -7,36 +7,42 @@ using VSCodeLauncher.Lib.Config;
 *   VSCodeLauncher.exe <FULL_PATH>
 */
 
-var cmdArgs = Environment.GetCommandLineArgs();
-var fullPath = CmdArgsParser.GetFullPath(cmdArgs);
-var op = CmdArgsParser.ResolveOpenPath(fullPath);
-if (op == null) {
-    // ここに来ることはない
-    // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
-    return;
+try {
+    var cmdArgs = Environment.GetCommandLineArgs();
+    var fullPath = CmdArgsParser.GetFullPath(cmdArgs);
+    var op = CmdArgsParser.ResolveOpenPath(fullPath);
+    if (op == null) {
+        // ここに来ることはない
+        // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
+        return;
+    }
+
+    var config = ConfigFile.LoadConfig("config.json");
+    if (config == null) {
+        // ここに来ることはない
+        // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
+        return;
+    }
+
+    var cg = new CommandGenerator(op, config);
+    if (cg == null) {
+        // ここに来ることはない
+        // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
+        return;
+    }
+
+    var psi = new ProcessStartInfo();
+    psi.FileName = config.CodePath;
+    psi.WindowStyle = ProcessWindowStyle.Hidden;
+
+    foreach (var argv in cg.GenerateCommand()) {
+        psi.ArgumentList.Add(argv);
+    }
+
+    Process.Start(psi);
+} catch (Exception ex) {
+    Console.WriteLine(ex.ToString());
+    Console.WriteLine("終了するには何かキーを入力してください。");
+    Console.ReadLine();
 }
-
-var config = ConfigFile.LoadConfig("config.json");
-if (config == null) {
-    // ここに来ることはない
-    // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
-    return;
-}
-
-var cg = new CommandGenerator(op, config);
-if (cg == null) {
-    // ここに来ることはない
-    // 例外が投げられてルートでキャッチされて勝手に死ぬ想定
-    return;
-}
-
-var psi = new ProcessStartInfo();
-psi.FileName = config.CodePath;
-psi.WindowStyle = ProcessWindowStyle.Hidden;
-
-foreach (var argv in cg.GenerateCommand()) {
-    psi.ArgumentList.Add(argv);
-}
-
-Process.Start(psi);
 
